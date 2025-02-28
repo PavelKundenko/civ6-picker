477f97a0edc921f9a5d4fc4b6e473c16b8abd10d
Pavlo Kundenko <pavel.kundenko@gmail.com>
2025-03-01 00:10:27 +0200

    fix: data fetching

--- src/app/civilizations/page.tsx
+++ src/app/civilizations/page.tsx
@@ -1,15 +1,21 @@
 import { type ICiv } from './models/civ';
 import { Container } from './Container';
+import fs from 'node:fs';
+import path from 'node:path';
+
+async function getLeadersData() {
+  const filePath = path.join(process.cwd(), 'data', 'leaders.json');
+  const jsonData = fs.readFileSync(filePath, 'utf8');
+
+  return JSON.parse(jsonData) as ICiv[];
+}
 
 export default async function Civilizations() {
-  const response = await fetch('http://localhost:3000/data/leaders.json');
-
-  const civs: ICiv[] = await response.json();
+  const civs = await getLeadersData();
 
   return (
     <section className='container mx-auto p-6'>
       <h1 className='mb-8 text-3xl font-bold'>Civilizations</h1>
-
       <Container civs={civs} />
     </section>
   );
